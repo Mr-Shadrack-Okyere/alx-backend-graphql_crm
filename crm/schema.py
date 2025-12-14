991e9d9cc172eeb7f6ab12048298499b146850d2
Shadrack Okyere <masterokyere100@gmail.com>
2025-12-14 19:07:24 +0000

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -1,32 +1,21 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Customer, Product, Order
-from graphene.types.generic import GenericScalar
 
-# DjangoObjectTypes
+# -----------------------
+# Customer Type
+# -----------------------
 class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
+        fields = ("id", "name", "email", "phone")
 
-class ProductType(DjangoObjectType):
-    class Meta:
-        model = Product
 
-class OrderType(DjangoObjectType):
-    class Meta:
-        model = Order
-
-# Queries
-class CRMQuery(graphene.ObjectType):
+# -----------------------
+# Query Class
+# -----------------------
+class Query(graphene.ObjectType):
     all_customers = graphene.List(CustomerType)
-    all_products = graphene.List(ProductType)
-    all_orders = graphene.List(OrderType)
 
     def resolve_all_customers(root, info):
         return Customer.objects.all()
-
-    def resolve_all_products(root, info):
-        return Product.objects.all()
-
-    def resolve_all_orders(root, info):
-        return Order.objects.all()
