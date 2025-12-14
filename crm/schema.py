7fab027566aef94124bc678b9ed5bca016a6565b
Shadrack Okyere <masterokyere100@gmail.com>
2025-12-14 19:03:34 +0000

    Create schema.py

---
+++ crm/schema.py
@@ -0,0 +1,32 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Customer, Product, Order
+from graphene.types.generic import GenericScalar
+
+# DjangoObjectTypes
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+
+# Queries
+class CRMQuery(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
+    all_products = graphene.List(ProductType)
+    all_orders = graphene.List(OrderType)
+
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
+
+    def resolve_all_products(root, info):
+        return Product.objects.all()
+
+    def resolve_all_orders(root, info):
+        return Order.objects.all()
