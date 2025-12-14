f579c21c888b7f6ce20d033ec9c6ae7626c60100
Shadrack Okyere <masterokyere100@gmail.com>
2025-12-14 18:59:40 +0000

    Update settings.py

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -1,1 +1,15 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
+    "graphene_django",  # Add graphene-django
+    "crm",              # Add your crm app
+]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema",  # Path to your schema
+}
