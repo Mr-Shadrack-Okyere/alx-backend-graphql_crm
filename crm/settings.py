c5a00cd005fec6bf93f964aeded679b03aeac10a
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 22:29:04 +0000

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -10,3 +10,10 @@ INSTALLED_APPS = [
     # existing apps
     'django_celery_beat',
 ]
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generatecrmreport',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
