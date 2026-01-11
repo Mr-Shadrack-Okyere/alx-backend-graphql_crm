23e9489e632ac7d3d210fed31841159f63432efe
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 22:22:38 +0000

    Update cron.py

--- crm/cron.py
+++ crm/cron.py
@@ -34,3 +34,47 @@ def log_crm_heartbeat():
 
     with open(LOG_FILE, "a") as file:
         file.write(message + "\n")
+
+from datetime import datetime
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+GRAPHQL_ENDPOINT = "http://localhost:8000/graphql"
+LOG_FILE = "/tmp/low_stock_updates_log.txt"
+
+
+def update_low_stock():
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    transport = RequestsHTTPTransport(
+        url=GRAPHQL_ENDPOINT,
+        verify=True,
+        retries=3,
+    )
+
+    client = Client(
+        transport=transport,
+        fetch_schema_from_transport=False,
+    )
+
+    mutation = gql("""
+        mutation {
+            updateLowStockProducts {
+                success
+                products {
+                    name
+                    stock
+                }
+            }
+        }
+    """)
+
+    result = client.execute(mutation)
+
+    products = result["updateLowStockProducts"]["products"]
+
+    with open(LOG_FILE, "a") as log:
+        for product in products:
+            log.write(
+                f"[{timestamp}] {product['name']} new stock: {product['stock']}\n"
+            )
